ab9f015f26b93437e2634e136877692fc31b8abc
Sanvir Dessai <sanvir@sovtech.com>
2021-07-20 23:33:32 +0200

    feat(canvas): add canvas component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Canvas } from './Components/canvas'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h>
+          Japanese OCR
+        </h>
+        <p style={{ fontSize: "18px", marginBottom: "50px"}}>
+            I've been spending some time learning Japanese and I wanted an easy way to test whether I could write the characters properly.
+            So here's a small Optical Character Recognition (OCR) web app which does just that (kind of &#128517;), hacked together with&nbsp;
+            <a href="https://reactjs.org/docs/create-a-new-react-app.html" target="_blank" rel="noopener noreferrer">CRA</a>, <a href="https://www.npmjs.com/package/react-sketch-canvas" target="_blank" rel="noopener noreferrer">react-sketch-canvas</a>
+            &nbsp;and <a href="https://github.com/naptha/tesseract.js" target="_blank" rel="noopener noreferrer">Tesseract.js</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Canvas />
       </header>
     </div>
   );
---
+++ src/Components/canvas.jsx
@@ -0,0 +1,109 @@
+import React, { useState, createRef } from "react";
+import { ReactSketchCanvas } from "react-sketch-canvas";
+import Tesseract from "tesseract.js"
+import logo from '../logo.svg'
+import './canvas.scss';
+
+const LANGUAGES = [
+    {
+        label: 'Japanese',
+        value: 'jpn'
+    },
+    {
+        label: 'English',
+        value: 'eng'
+    },
+    {
+        label: 'Greek',
+        value: 'ell'
+    },
+    {
+        label: 'Sanskrit',
+        value: 'san'
+    },
+    {
+        label: 'Chinese',
+        value: 'chi_sim'
+    },
+    {
+        label: 'Hindi',
+        value: 'hin'
+    }
+]
+
+export const Canvas = () => {
+
+    const canvas = createRef()
+    const [text, setText] = useState("")
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const [language, setLanguage] = useState("jpn")
+
+    return (
+        <>
+            <div className="Canvas-container">
+                <ReactSketchCanvas
+                    ref={canvas}
+                    strokeWidth={5}
+                    strokeColor="black"
+                    height={500}
+                    width="80%"
+                    style={{ marginLeft: "10%" }}
+                />
+                <select
+                    className="btn"
+                    name="languages"
+                    id="languages"
+                    onChange={event => {
+                        setLanguage(event.value)
+                    }}
+                >
+                    {LANGUAGES.map(option => {
+                        return <option value={option.value}>{option.label}</option>
+                    })}
+                </select>
+                <button
+                    className="btn draw-border"
+                    onClick={() => {
+                        setLoading(true)
+                        canvas.current
+                            .exportImage("png")
+                            .then(data => {
+                                Tesseract.recognize(
+                                    data,
+                                    language
+                                ).then(result => {
+                                    setText(result.data.text)
+                                    setError(null)
+                                    setLoading(false)
+                                })
+                            })
+                            .catch(error => {
+                                setError(error)
+                                setLoading(false)
+                            });
+                    }}
+                >
+                    Recognise
+                </button>
+                <button
+                    className="btn draw-border"
+                    onClick={() => {
+                        setLoading(false)
+                        setText("")
+                        canvas.current.clearCanvas()
+                    }}
+                >
+                    Clear
+                </button>
+            </div>
+           
+            {loading
+                ? <img src={logo} className="App-logo" alt="logo" />
+                : <div className="Text-container">
+                    <p style={{ fontSize: "60px"}}> {error ? error : text}</p>
+                </div>
+            }
+        </>
+    );
+}
